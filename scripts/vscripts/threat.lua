c576851feb41975e255d0a2593b826e4deec67c0
Xavier Tseng <364671459@qq.com>
2014-07-29 12:01:39 +0800

    加入仇恨系统，待测试

---
+++ scripts/vscripts/threat.lua
@@ -0,0 +1,112 @@
+if ThreatSystem == nil then
+  ThreatSystem = {}
+end
+
+function ThreatSystem:new(o)
+	o = o or {}
+	setmetatable(o, self)
+	return o
+end
+
+function ThreatSystem:NewThreat( unit , target )
+	self.Aggro[target] = self.Aggro[target] or {}
+	self.Aggro[target][unit] = 0
+end
+
+function ThreatSystem:AddAggro( unit , target , amount)
+	if self.Aggro[target].keep then return end
+	self.Aggro[target][unit] = self.Aggro[target][unit] or 0
+	self.Aggro[target][unit] = self.Aggro[target][unit] + amount
+end
+
+function ThreatSystem:ReduceAggro( unit , target , amount )
+	if self.Aggro[target].keep then return end
+	self.Aggro[target][unit] = self.Aggro[target][unit] or 0
+	self.Aggro[target][unit] = self.Aggro[target][unit] - amount
+end
+
+function ThreatSystem:RefreshAggroTable(tbl)
+	for k,v in pairs(tbl) do
+		if not k:IsValidEntity() then
+			v = 0
+			break
+		end
+		if not k:IsAlive() then
+			v = 0
+			break
+		end
+	end
+end
+
+function ThreatSystem:GetAggro( target )
+	-- ensure the unit is alive
+	self:RefreshAggroTable(self.Aggro[target])
+
+	-- return the aggro table
+	return self.Aggro[target]
+end
+
+function ThreatSystem:GetAggroAtSquence( target , squence)
+	-- ensure the unit is alive
+	self:RefreshAggroTable(self.Aggro[target])
+
+	if not self.Aggro[target] then
+		tPrint('ERROR: target aggro not defined')
+		return
+	end
+	local targetAggro = self.Aggro[target]
+
+	local sortFunc = function(a, b) return b < a end
+	table.sort( targetAggro , sortFunc )
+
+	local returnAggro = {}
+	local od = 1
+	for k,_ in pairs(targetAggro) do
+		returnAggro[od] = k
+		od = od + 1
+	end
+	return returnAggro[squence]
+end
+
+function ThreatSystem:ClearAggro( target )
+	self.Aggro[target] = {}
+end
+
+function ThreatSystem:KeepAggro( target )
+	self.Aggro[target].keep = self.Aggro[target].keep or {}
+	self.Aggro[target].keep = true
+end
+
+function AddAggro( keys )
+	tPrintTable(keys)
+	if not keys.AggroUnit then
+		tPrint( 'Aggro Unit Not Defined in ability' )
+		return
+	end
+	local target = keys.target_entindex or WardenGameMode.CurrentBossData.unit or 'empty'
+	target = EntIndexToHScript(target)
+	local unit = keys.caster_entindex or keys.attacker or 'empty'
+	unit = EntIndexToHScript(unit)
+	local amount = keys.amount or 0
+
+	if not ThreatSystem.Aggro[target][unit] then
+		ThreatSystem:NewThreat( unit , target )
+	end
+
+	ThreatSystem:AddAggro( unit, target , amount )
+end
+
+function ReduceAggro( keys )
+	tPrintTable(keys)
+	local target = keys.target_entindex or WardenGameMode.CurrentBossData.unit or 'empty'
+	target = EntIndexToHScript(target)
+	local unit = keys.caster_entindex or keys.attacker or 'empty'
+	unit = EntIndexToHScript(unit)
+	local amount = keys.amount or 0
+
+	if not ThreatSystem.Aggro[target][unit] then
+		ThreatSystem:NewThreat( unit , target )
+	end
+
+	ThreatSystem:ReduceAggro( unit, target , amount )
+end
